fdb64b2c945d4132fefdcb120514d57b598cb30e
Tolik Pylypchuk <pylypchuk.tolik@gmail.com>
2023-01-15 20:22:22 +0200

    Implement the progress bar component

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,50 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const STYLES = {
+  'large': {
+    '--height': '24px',
+    '--padding': '4px'
+  },
+  'medium': {
+    '--height': '12px',
+    '--padding': '0'
+  },
+  'small': {
+    '--height': '8px',
+    '--padding': '0'
+  }
+}
+
+const ProgressBarWrapper = styled.div`
+  height: var(--height);
+  padding: var(--padding);
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: 4px;
+`;
+
+const ProgressBarContentWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 4px;
+  overflow: clip;
+`;
+
+const ProgressBarContent = styled.div`
+  width: ${p => p.value}%;
+  height: 100%;
+  background-color: ${COLORS.primary};
+`;
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const style = STYLES[size];
+  return (
+    <ProgressBarWrapper role="progressbar" aria-valuenow={value} style={style}>
+      <ProgressBarContentWrapper>
+        <ProgressBarContent value={value} />
+      </ProgressBarContentWrapper>
+    </ProgressBarWrapper>
+  );
 };
 
 export default ProgressBar;
